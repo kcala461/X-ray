21ad3972b2887f32fdd780c2be1fa92be13e50aa
XahepGomez <48773063+XahepGomez@users.noreply.github.com>
2019-06-03 22:45:28 -0500

    Update matriz_confusion.py

--- matriz_confusion.py
+++ matriz_confusion.py
@@ -1,30 +1,11 @@
 from sklearn.metrics import confusion_matrix
 
-y_true = ["Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia"]
-y_pred = ["Neumonia","Neumonia","Normal","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia"]
+y_true = ["Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Normal","Normal","Normal","Normal","Neumonia","Neumonia","Neumonia","Normal","Normal","Neumonia","Neumonia"]
+y_pred = ["Neumonia","Neumonia","Normal","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Neumonia","Normal","Normal","Normal","Normal","Neumonia","Neumonia","Neumonia","Normal","Normal","Neumonia","Neumonia"]
 
-y_verdad= ["Normal",
-"Normal",
-"Normal",
-"Normal",
-"Neumonia",
-"Neumonia",
-"Neumonia",
-"Normal",
-"Normal",
-"Neumonia",
-"Neumonia"]
 
-y_prueba = ["Normal",
-"Normal",
-"Normal",
-"Normal",
-"Neumonia",
-"Neumonia",
-"Neumonia",
-"Normal",
-"Normal",
-"Neumonia",
-"Neumonia"]
+#Otra muestra
+y_true1= ["Normal","Normal","Normal","Normal","Neumonia","Neumonia","Neumonia","Normal","Normal","Neumonia","Neumonia"]
+y_pred1 = ["Normal","Normal","Normal","Normal","Neumonia","Neumonia","Neumonia","Normal","Normal","Neumonia","Neumonia"]
 
-print(confusion_matrix(y_verdad, y_prueba))
+print(confusion_matrix(y_true, y_pred))
